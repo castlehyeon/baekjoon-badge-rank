2ce4828a87a5a3f4c15de9cd9d0136b934487a10
castlehyeon <qnftktls118@naver.com>
2023-05-07 00:38:39 +0900

    springboot dependency settings

---
+++ src/main/java/com/project/bbr/BbrApplication.java
@@ -0,0 +1,13 @@
+package com.project.bbr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/bbr/BbrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bbr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
